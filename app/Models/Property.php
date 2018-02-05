d1c06bb2fb13ea7cdb6c442d5659819fd094c59c
Kenneth Sok <kenneth.sok@gmail.com>
2018-02-05 04:53:41 +0000

    (#5) Add dependencies and libraries for setting up
    Added Laravelcollective and Presenters libraries.
    
    Updated from Kareia-Dash for Property.php.

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -3,8 +3,64 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Laracasts\Presenter\PresentableTrait;
 
 class Property extends Model
 {
-    //
+    use SoftDeletes;
+    use PresentableTrait;
+
+    protected $presenter = 'App\Models\Property\PropertyPresenter';
+
+    protected $table = "properties";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'street_address',
+        'city',
+        'state_id',
+        'zip',
+        'bedrooms',
+        'bathrooms',
+        'garages',
+        'year_built',
+        'living_square_footage',
+        'lot_square_footage',
+        'neighborhood',
+    ];
+
+    /**
+     * Property constructor.
+     *
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+    }
+
+    /**
+     * Gets the state associated with this property
+     */
+    public function state()
+    {
+        return $this->belongsTo('App\Models\States\State');
+    }
+
+    /**
+     * Foreign key for user.
+     *
+     * @return HasOne
+     */
+    public function user()
+    {
+        return $this->hasOne('App\User');
+    }
 }
