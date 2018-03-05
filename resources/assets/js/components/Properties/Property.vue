8fc646183d729c467c4468f8f734120a7d6f880d
Kenneth Sok <kenneth.sok@gmail.com>
2018-03-05 02:53:45 +0000

    (#5) Saving progress for Property.vue
    Made some cosmetic changes. Saving here.

--- resources/assets/js/components/Properties/Property.vue
+++ resources/assets/js/components/Properties/Property.vue
@@ -1,16 +1,62 @@
 <template>
     <div class="panel panel-default">
-        <div class="panel-body">
-            <!-- todo: Add property information here -->
-            <p>{{ prop.street_address }}</p>
+        <div v-if="!editing">
+            <div class="panel-body">
+                <div class="row">
+                    <label class="col-lg-4 control-label" for="street_address">Street Address</label>
+                    <p class="col-lg-8">{{ prop.street_address }}</p>
+                </div>
+                <div class="row">
+                    <label class="col-lg-4 control-label" for="city">City</label>
+                    <p class="col-lg-8">{{ prop.city }}</p>
+                </div>
+                <div class="row">
+                    <label class="col-lg-4 control-label" for="zip">Zip</label>
+                    <p class="col-lg-8">{{ prop.zip }}</p>
+                </div>
+            </div>
+            <div class="panel-footer">
+                <a @click="onEdit">Edit</a> |
+                <a @click="onDelete">Delete</a>
+            </div>
         </div>
-        <div class="panel-footer">
-            <a href="" @click="onUpdate">Save</a> |
-            <a href="" @click="onCancel">Cancel</a>
-        </div>
-        <div class="panel-footer">
-            <a href="" @click="onEdit">Edit</a> |
-            <a href="" @click="onDelete">Delete</a>
+        <div v-if="editing">
+            <div class="panel-body">
+                <div class="form-group">
+                    <label class="col-lg-4 control-label" for="street_address">Street Address</label>
+                    <div class="col-lg-8">
+                        <input
+                            type="text"
+                            id="street_address"
+                            class="form-control"
+                        >
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-4 control-label" for="city">City</label>
+                    <div class="col-lg-8">
+                        <input
+                                type="text"
+                                id="city"
+                                class="form-control"
+                        >
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-lg-4 control-label" for="zip">Zip</label>
+                    <div class="col-lg-8">
+                        <input
+                                type="text"
+                                id="zip"
+                                class="form-control"
+                        >
+                    </div>
+                </div>
+            </div>
+            <div class="panel-footer">
+                <a @click="onUpdate">Save</a> |
+                <a @click="onCancel">Cancel</a>
+            </div>
         </div>
     </div>
 </template>
@@ -18,10 +64,35 @@
 <script>
     export default {
         name: "property",
-        props: ['prop']
+        props: ['prop'],
+        data() {
+            return {
+                editing: false,
+                editValues: {
+                    street_address: this.prop.street_address,
+                    city: this.prop.city,
+                    zip: this.prop.zip
+                }
+            }
+        },
+        methods: {
+            onEdit() {
+                this.editing = true;
+                this.editValues = {
+                    street_address: this.prop.street_address,
+                    city: this.prop.city,
+                    zip: this.prop.zip
+                }
+            },
+            onCancel() {
+                this.editing = false;
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    a {
+        cursor: pointer;
+    }
 </style>
